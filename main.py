26f7830179e5cfb458b7f59f18ab23886158e402
Zilant1337 <grigorenko_p@inbox.ru>
2024-11-06 20:34:12 +0300

    Task 1: Added colour inversion

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import numpy
 import numpy as np
 import PIL
 from matplotlib import pyplot as plt
 
-a = 1
+a = np.array(0)
+
 # Импорт в массив
-with PIL.Image.open("KOS-MOS.jpg") as img:
+with PIL.Image.open("helldiver.jpg") as img:
     a = np.array(img)
 plt.imshow(a)
+plt.show()
+
+# Inverting colour
+invert_tensor = np.full(a.shape,255)
+a= invert_tensor-a
+plt.imshow(a)
+plt.show()
+
+# Grayscaling the image
