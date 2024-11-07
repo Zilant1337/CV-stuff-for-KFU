0f7c8e34a0c4a125e3c54a5d971a214146db88cb
Zilant1337 <grigorenko_p@inbox.ru>
2024-11-07 11:24:25 +0300

    Task 1: Added histograms

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ with PIL.Image.open("helldiver.jpg") as img:
 plt.imshow(a)
 plt.show()
 
+
 # Inverting colour
 invert_tensor = np.full(a.shape,255)
 a= invert_tensor-a
 plt.imshow(a)
 plt.show()
 
+()
 # Greyscaling the image
 grey_a = np.full((a.shape[0],a.shape[1]),0, dtype=np.float64)
 for i in range (3):
@@ -27,11 +29,19 @@ for i in range (3):
 plt.imshow(grey_a,cmap='gray')
 plt.show()
 
+hist = np.histogram(grey_a,bins=range(0,255))
+plt.bar(x=hist[1][1:], height = hist[0])
+plt.show()
 # Adding random noise
 noise_matrix = np.random.normal(loc = 50,scale=50,size=grey_a.shape)
 grey_a+=noise_matrix
 plt.imshow(grey_a,cmap='gray')
 plt.show()
+
+hist = np.histogram(grey_a,bins=range(0,255))
+plt.bar(x=hist[1][1:], height = hist[0])
+plt.show()
+# Gaussian filtering
 
 
 
