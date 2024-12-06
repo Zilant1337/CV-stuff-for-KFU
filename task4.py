784cf18d8e6ea030a0fe622011c62a6b42ca4b95
Zilant1337 <grigorenko_p@inbox.ru>
2024-12-06 14:03:31 +0300

    Task 4: started work

--- task4.py
+++ task4.py
@@ -22,7 +22,7 @@ for x in range(0, gaussian_kernel_size):
 
 
 # Importing image into a list
-with PIL.Image.open("helldiver.jpg") as img:
+with PIL.Image.open("lanes.jpg") as img:
     a = np.array(img)
 plt.imshow(a)
 plt.show()
@@ -38,19 +38,19 @@ plt.show()
 # Gaussian filtering
 
 bordered_shape = [x + gaussian_kernel_size * 2 for x in grey_a.shape]
-bordered_gray_a = np.full(bordered_shape,0)
-bordered_gray_a[gaussian_kernel_size:gaussian_kernel_size + grey_a.shape[0], gaussian_kernel_size:gaussian_kernel_size + grey_a.shape[1]] = grey_a
+bordered_cummulative = np.full(bordered_shape, 0)
+bordered_cummulative[gaussian_kernel_size:gaussian_kernel_size + grey_a.shape[0], gaussian_kernel_size:gaussian_kernel_size + grey_a.shape[1]] = grey_a
 
-gaussian_blur_a = np.full(grey_a.shape,0)
-for i in range(gaussian_kernel_size,bordered_gray_a.shape[0]-gaussian_kernel_size):
-    for j in range(gaussian_kernel_size,bordered_gray_a.shape[1]-gaussian_kernel_size):
-        slice_for_kernel = bordered_gray_a[i-gaussian_kernel_size//2:i+gaussian_kernel_size//2+1,j-gaussian_kernel_size//2:j+gaussian_kernel_size//2+1]
+gaussian_blur_cummulative = np.full(grey_a.shape, 0)
+for i in range(gaussian_kernel_size, bordered_cummulative.shape[0] - gaussian_kernel_size):
+    for j in range(gaussian_kernel_size, bordered_cummulative.shape[1] - gaussian_kernel_size):
+        slice_for_kernel = bordered_cummulative[i - gaussian_kernel_size // 2:i + gaussian_kernel_size // 2 + 1, j - gaussian_kernel_size // 2:j + gaussian_kernel_size // 2 + 1]
         slice_for_kernel = np.multiply(slice_for_kernel,kernel)
         # print(f"X: {i-gaussian_kernel_size}, Y: {j-gaussian_kernel_size}")
-        gaussian_blur_a[i-gaussian_kernel_size, j-gaussian_kernel_size] = np.sum(slice_for_kernel)
+        gaussian_blur_cummulative[i - gaussian_kernel_size, j - gaussian_kernel_size] = np.sum(slice_for_kernel)
 
 
-plt.imshow(gaussian_blur_a,cmap='gray')
+plt.imshow(gaussian_blur_cummulative, cmap='gray')
 plt.show()
 
 # Calculating gradient
@@ -67,12 +67,12 @@ rounded_gradient_directions_radians = np.zeros(grey_a.shape)
 nms_grey_a = np.zeros(grey_a.shape)
 
 bordered_shape = [x + 2 for x in grey_a.shape]
-bordered_gray_a = np.full(bordered_shape,0)
-bordered_gray_a[1:1 + grey_a.shape[0], 1:1 + grey_a.shape[1]] = grey_a
+bordered_cummulative = np.full(bordered_shape, 0)
+bordered_cummulative[1:1 + grey_a.shape[0], 1:1 + grey_a.shape[1]] = grey_a
 
-for i in range(1,bordered_gray_a.shape[0]-1):
-    for j in range(1,bordered_gray_a.shape[1]-1):
-        slice_for_kernel = bordered_gray_a[i-1:i+2,j-1:j+2]
+for i in range(1, bordered_cummulative.shape[0] - 1):
+    for j in range(1, bordered_cummulative.shape[1] - 1):
+        slice_for_kernel = bordered_cummulative[i - 1:i + 2, j - 1:j + 2]
         x_gradient = np.sum(np.multiply(x_kernel,slice_for_kernel))
         y_gradient = np.sum(np.multiply(y_kernel,slice_for_kernel))
 
@@ -147,21 +147,94 @@ def border_specification(gradient, t_high, t_low):
 
 specified_borders = border_specification(nms_grey_a,t_high,t_low)
 
-fig,axis = plt.subplots(2,4)
-axis[0,0].imshow(grey_a,cmap='gray')
-axis[0,0].set_title("Greyscale image")
-axis[0,1].imshow(gradient_magnitude,cmap='gray')
-axis[0,1].set_title("Magnitude")
-axis[0,2].imshow(gradient_x, cmap= 'gray')
-axis[0,2].set_title("Gradient X")
-axis[0,3].imshow(gradient_y, cmap= 'gray')
-axis[0,3].set_title("Gradient Y")
-axis[1,0].imshow(rounded_gradient_directions_radians, cmap='gray')
-axis[1,0].set_title("Rounded gradient Directions")
-axis[1,1].imshow(gradient_directions, cmap='gray')
-axis[1,1].set_title("Gradient Directions")
-axis[1,2].imshow(nms_grey_a, cmap='gray')
-axis[1,2].set_title("Non-maximum supressed gradient")
-axis[1,3].imshow(specified_borders,cmap='gray')
-axis[1,3].set_title("Specified borders")
+def hough_transform(img):
+    diagonal = int(math.sqrt(img.shape[0]**2+img.shape[1]**2))
+    rhos = np.linspace(0, diagonal, diagonal)
+    thetas = np.deg2rad(np.arange(-90,180,1))
+
+    cummulative = np.zeros((len(rhos), len(thetas)), dtype=np.float32)
+
+    for x in range(img.shape[0]):
+        for y in range (img.shape[1]):
+            if img[x,y]==255:
+                for id, theta in enumerate(thetas):
+                    rho = int(y*np.cos(theta)+ x* np.sin(theta))
+                    cummulative[rho,id] +=1
+
+    return cummulative, rhos,thetas
+
+cummulative,rhos,thetas = hough_transform(specified_borders)
+
+print("Please input the size of the kernel for cummulative:")
+kernel_size_input = int(input())
+print("Please input the gaussian standart deviation:")
+gaussian_standart_deviation = float(input())
+print("Please input the threshold for non-maximum suppression:")
+nms_threshold = float(input())
+gaussian_kernel_size = kernel_size_input - (1 - kernel_size_input % 2)
+
+# Creating kernel
+kernel = np.zeros((gaussian_kernel_size,gaussian_kernel_size))
+for x in range(0, gaussian_kernel_size):
+    for y in range(0, gaussian_kernel_size):
+        x_from_center = gaussian_kernel_size//2+1-x
+        y_from_center = gaussian_kernel_size // 2 + 1 - y
+        kernel[x,y]=1/(2*math.pi*gaussian_standart_deviation**2)*math.exp(-1*(x_from_center**2+y_from_center**2)/(2*gaussian_standart_deviation**2))
+
+# Gaussian filtering
+
+bordered_shape = [x + gaussian_kernel_size * 2 for x in cummulative.shape]
+bordered_cummulative = np.full(bordered_shape, 0)
+bordered_cummulative[gaussian_kernel_size:gaussian_kernel_size + cummulative.shape[0], gaussian_kernel_size:gaussian_kernel_size + cummulative.shape[1]] = cummulative
+
+gaussian_blur_cummulative = np.full(cummulative.shape, 0)
+for i in range(gaussian_kernel_size, bordered_cummulative.shape[0] - gaussian_kernel_size):
+    for j in range(gaussian_kernel_size, bordered_cummulative.shape[1] - gaussian_kernel_size):
+        slice_for_kernel = bordered_cummulative[i - gaussian_kernel_size // 2:i + gaussian_kernel_size // 2 + 1, j - gaussian_kernel_size // 2:j + gaussian_kernel_size // 2 + 1]
+        slice_for_kernel = np.multiply(slice_for_kernel,kernel)
+        # print(f"X: {i-gaussian_kernel_size}, Y: {j-gaussian_kernel_size}")
+        gaussian_blur_cummulative[i - gaussian_kernel_size, j - gaussian_kernel_size] = np.sum(slice_for_kernel)
+
+def nms(cummulative, threshold):
+    max_value = np.max(cummulative)
+    significant_value = int(threshold * max_value)
+    cummulative_height, cummulative_width = cummulative.shape
+
+    local_maximum = []
+    for r in range(cummulative_height):
+        for t in range(cummulative_width):
+            if cummulative[r, t] > significant_value:
+                neighborhood = cummulative[max(0, r - 1):min(cummulative.shape[0], r + 2), max(0, t - 1):min(cummulative.shape[1], t + 2)]
+                if cummulative[r, t] == np.max(neighborhood):
+                    local_maximum.append((r, t, cummulative[r, t]))
+
+    print(f"Found {len(local_maximum)} lines with threshold {threshold * 100}% from maximum value")
+
+    return local_maximum
+
+nms_blurred_cummulative = nms(gaussian_blur_cummulative,nms_threshold)
+
+def draw_lines(img, rhos, thetas, nms_cummulative, accuracy=0.8):
+    height, width, _ = img.shape
+    for r, t, value in nms_cummulative:
+        theta = thetas[t]
+        rho = rhos[r]
+
+        for y in range(height):
+            for x in range(width):
+                if abs(x * np.cos(theta) + y * np.sin(theta) - rho) < accuracy:
+                    img[y, x] = [0, 0, 255]
+
+    return img
+
+a_with_lines = draw_lines(a,rhos,thetas,nms_blurred_cummulative)
+
+fig,axis = plt.subplots(1,3)
+axis[0].imshow(specified_borders,cmap='gray')
+axis[0].set_title("Specified borders (Canny)")
+axis[1].imshow(gaussian_blur_cummulative, cmap='gray')
+axis[1].set_title("Cummulative array")
+axis[2].imshow(a_with_lines)
+axis[2].set_title("Image with found lines")
+
 plt.show()
